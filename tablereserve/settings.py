863525027ea4bfac5df0493e84fbff1c104f7acb
olegshiryaev <oashiryaev@yandex.ru>
2025-12-12 10:01:03 +0300

    Обновление моделей, форм и представлений для системы бронирования

--- tablereserve/settings.py
+++ tablereserve/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import environ
 
 env = environ.Env()
-environ.Env.read_env(env_file=Path("./docker/env/.env.dev"))
+environ.Env.read_env(env_file=Path("./docker/env/.env.prod"))
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
